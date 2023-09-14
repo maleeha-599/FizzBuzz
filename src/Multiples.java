180338d62c24fc26d2860d488fbfa25f9b2ab441
rahman <maleeha.rahman@mail.utoronto.ca>
2023-09-14 17:14:33 -0400

    Created program to determing number of multiples of 3 and 5

---
+++ src/Multiples.java
@@ -0,0 +1,21 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 0;
+        for (int z = 1; z < 1000; z++) {
+            boolean divisibleBy3 = z % 3 == 0;
+            boolean divisibleBy5 = z % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+                i = i + 1;
+            }
+            else if (divisibleBy3) {
+                i = i + 1;
+            }
+            else if (divisibleBy5) {
+                i = i + 1;
+            }
+        }
+
+        System.out.print(i);
+    }
+}
